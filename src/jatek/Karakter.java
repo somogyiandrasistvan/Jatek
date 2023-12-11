ed1c837c2ee9e25cb4ba17efca0940ec9a04fa0a
somogyiandrasistvan <somogyiandraska@gmail.com>
2023-12-11 13:25:32 +0100

    karakter adattagok elkeszitese

--- src/jatek/Karakter.java
+++ src/jatek/Karakter.java
@@ -1,5 +1,16 @@
 package jatek;
 
+import java.util.Random;
+
 public class Karakter {
+
+    private int eletero, ugyesseg, szerencse;
+    Random rnd;
+
+    public Karakter() {
+        eletero = rnd.nextInt(1, 7) + rnd.nextInt(1, 7) + 12;
+        ugyesseg = rnd.nextInt(1, 7) + 6;
+        szerencse = rnd.nextInt(1, 7) + 6;
+    }
 
 }
